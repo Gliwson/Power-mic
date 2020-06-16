af281d1b3d9a2525cd8f8d1e6678d9da38e26e30
Mateusz Gliwka <gliwson@gmail.com>
2020-06-16 23:16:50 +0200

    backend add Keycloak

--- backend/src/main/java/pl/power/PowerStationApplication.java
+++ backend/src/main/java/pl/power/PowerStationApplication.java
@@ -2,16 +2,21 @@ package pl.power;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 //@Controller
 @SpringBootApplication
-public class PowerStationApplication {
+public class PowerStationApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PowerStationApplication.class, args);
     }
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(PowerStationApplication.class);
+    }
 //    @GetMapping(value = "/{path:[^\\.]*}")
 //    public String redirect() {
 //        return "forward:/";
--- backend/src/main/java/pl/power/controller/PowerStationController.java
+++ backend/src/main/java/pl/power/controller/PowerStationController.java
@@ -5,11 +5,13 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 import pl.power.model.PowerStationDTO;
 import pl.power.services.PowerStationService;
 import pl.power.services.impl.PairPageable;
 
+import javax.annotation.security.RolesAllowed;
 import javax.validation.Valid;
 import java.math.BigDecimal;
 import java.util.Map;
@@ -24,6 +26,7 @@ public class PowerStationController {
         this.powerStationService = powerStationService;
     }
 
+    @RolesAllowed({"ROLE_USER","ROLE_ADMIN"})
     @GetMapping
     public Page<PowerStationDTO> getPowerStations(@RequestParam(name = "page", defaultValue = "0") int page,
                                                   @RequestParam(name = "size", defaultValue = "20") int size) {
@@ -32,38 +35,45 @@ public class PowerStationController {
         return new PageImpl<>(pairPageable.getElements(), pageRequest, pairPageable.getTotalElements());
     }
 
+    @RolesAllowed({"ROLE_USER","ROLE_ADMIN"})
     @GetMapping("/{id}")
     public PowerStationDTO getPowerStation(@PathVariable("id") Long id) {
         return powerStationService.findById(id);
     }
 
+    @RolesAllowed({"ROLE_USER","ROLE_ADMIN"})
     @PostMapping
     @ResponseStatus(value = HttpStatus.CREATED)
     public Long createPowerStation(@Valid @RequestBody PowerStationDTO powerStationDTO) {
         return powerStationService.save(powerStationDTO);
     }
 
+    @Secured("ROLE_ADMIN")
     @DeleteMapping("/{id}")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void deletePowerStation(@PathVariable Long id) {
         powerStationService.delete(id);
     }
 
+    @RolesAllowed({"ROLE_USER","ROLE_ADMIN"})
     @PatchMapping("/{id}")
     public PowerStationDTO updatePowerStation(@PathVariable Long id, @Valid @RequestBody PowerStationDTO powerStationDTO) {
         return powerStationService.update(id, powerStationDTO);
     }
 
+    @RolesAllowed({"ROLE_USER","ROLE_ADMIN"})
     @GetMapping("/{id}/{taskType}")
     public Long getNumberOfEvents(@PathVariable Long id, @PathVariable String taskType) {
         return powerStationService.countEventsByIdPowerStation(id, taskType);
     }
 
+    @RolesAllowed({"ROLE_USER","ROLE_ADMIN"})
     @GetMapping("/")
     public Map<Long, BigDecimal> getDateAndPower(@RequestParam(value = "date") String date) {
         return powerStationService.getDateAndPower(date);
     }
 
+    @Secured("ROLE_ADMIN")
     @GetMapping("/addAll")
     @ResponseStatus(value = HttpStatus.CREATED)
     public void addAll() {
