6e082c7ae7cabfb952be4fa2376d67fac443a42b
Mateusz Gliwka <mateusz.gliwka@ttsw.com.pl>
2020-07-09 12:45:00 +0200

    update app

--- authorization-server/src/main/java/pl/gliwson/auth/config/RegularJsonConfigProviderFactory.java
+++
@@ -1,7 +0,0 @@
-package pl.gliwson.auth.config;
-
-import org.keycloak.services.util.JsonConfigProviderFactory;
-
-public class RegularJsonConfigProviderFactory extends JsonConfigProviderFactory {
-
-}
