32a55993890d8472887f961c6551bfa8678efc6b
Mateusz Gliwka <mateusz.gliwka@ttsw.com.pl>
2020-07-10 16:16:50 +0200

    added FeignClient

--- backend/src/main/java/pl/power/PowerStationApplication.java
+++ backend/src/main/java/pl/power/PowerStationApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication
+@EnableFeignClients
 public class PowerStationApplication extends SpringBootServletInitializer implements RepositoryRestConfigurer {
 
     public static void main(String[] args) {
---
+++ backend/src/main/java/pl/power/repository/PowerStationWithRemoteServer.java
@@ -0,0 +1,8 @@
+package pl.power.repository;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient("")
+public interface PowerStationWithRemoteServer {
+
+}
